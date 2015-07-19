8832cfbc6441481a8721608121b97a5816ddc516
Suman Barick <codotronix@gmail.com>
2015-07-19 14:35:38 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -15,10 +15,36 @@ mainApp.controller('mainCtrl', ['$scope', 'sharedVars', function ($scope, shared
 
 
 /***** Routing Controllers ***********/
+mainApp.controller('homeCtrl', ['$scope', 'sharedVars', function ($scope, sharedVars) {
+
+}]);
+
+mainApp.controller('aboutCtrl', ['$scope', 'sharedVars', function ($scope, sharedVars) {
+	
+}]);
+
 mainApp.controller('peopleCtrl', ['$scope', 'sharedVars', function ($scope, sharedVars) {
    //$('body').removeClass().addClass('generalPage');
    sharedVars.changePageID('people');
 	$scope.items = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6', 'Item 7', 'Item 8', 'Item 9', 'Item 10', 'Item 11', 'Item 12'];
    
+}]);
+
+/********************* Projects Controller ***************************/
+mainApp.controller('projectsCtrl', ['$scope', 'sharedVars', function ($scope, sharedVars) {
+	$scope.projects = [
+		{"name": "Cool Project 1", "desc": "This is a very cool project. But dummy One..."},
+		{"name": "Cool Project 2", "desc": "This is a very cool project. But dummy One..."},
+		{"name": "Cool Project 3", "desc": "This is a very cool project. But dummy One..."},
+		{"name": "Cool Project 4", "desc": "This is a very cool project. But dummy One..."},
+		{"name": "Cool Project 5", "desc": "This is a very cool project. But dummy One..."},
+		{"name": "Cool Project 6", "desc": "This is a very cool project. But dummy One..."},
+		{"name": "Cool Project 7", "desc": "This is a very cool project. But dummy One..."},
+		{"name": "Cool Project 8", "desc": "This is a very cool project. But dummy One..."},
+		{"name": "Cool Project 9", "desc": "This is a very cool project. But dummy One..."},
+		{"name": "Cool Project 10", "desc": "This is a very cool project. But dummy One..."},
+		{"name": "Cool Project 11", "desc": "This is a very cool project. But dummy One..."},
+		{"name": "Cool Project 12", "desc": "This is a very cool project. But dummy One..."}
+	];
 }]);
 
